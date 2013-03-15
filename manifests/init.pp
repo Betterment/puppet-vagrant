20708443222c6300ec6beebb01e73b843ad42150
dustinlucien <dustin.lucien@gmail.com>
2013-03-15 13:33:42 -0400

    changed class to vagrant

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class subversion {
+class vagrant {
 	package { 'Vagrant': 
 		provider => 'pkgdmg',
 		source	=> 'http://files.vagrantup.com/packages/194948999371e9aee391d13845a0bdeb27e51ac0/Vagrant.dmg'
